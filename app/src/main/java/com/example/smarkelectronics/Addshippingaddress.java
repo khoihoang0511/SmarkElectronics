ea140da27925dd9a7e2492c29f5364b507fe1dd2
Thai1812 <130356756+Thai1812@users.noreply.github.com>
2023-11-02 19:10:53 +0700

    Add shipping address

--- app/src/main/java/com/example/smarkelectronics/Addshippingaddress.java
+++ app/src/main/java/com/example/smarkelectronics/Addshippingaddress.java
@@ -3,12 +3,17 @@ package com.example.smarkelectronics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class Addshippingaddress extends AppCompatActivity {
+
+    private Spinner spinnerAddshippingaddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addshippingaddress);
+
+        spinnerAddshippingaddress = findViewById(R.id.spinnerAddshippingaddress);
     }
 }
