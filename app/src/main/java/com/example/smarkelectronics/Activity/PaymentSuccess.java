4bbc8a2afa72c56d3bff537f25a6d0163949d71b
Thai1812 <130356756+Thai1812@users.noreply.github.com>
2023-11-07 15:33:50 +0700

    Add shipping address

--- app/src/main/java/com/example/smarkelectronics/Activity/PaymentSuccess.java
+++ app/src/main/java/com/example/smarkelectronics/Activity/PaymentSuccess.java
@@ -1,19 +1,16 @@
 package com.example.smarkelectronics.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
 import com.example.smarkelectronics.R;
 
-
-public class Addshippingaddress extends AppCompatActivity {
+public class PaymentSuccess extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addshippingaddress);
-
-
+        setContentView(R.layout.activity_payment_success);
     }
-
-
 }
