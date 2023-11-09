f6a34e3d51703fb93fc33e35b39813a21c124037
Thai1812 <130356756+Thai1812@users.noreply.github.com>
2023-11-09 18:17:02 +0700

    cart

--- app/src/main/java/com/example/smarkelectronics/Activity/Addshippingaddress.java
+++ app/src/main/java/com/example/smarkelectronics/Activity/Addshippingaddress.java
@@ -12,7 +12,11 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.smarkelectronics.R;
+import com.example.smarkelectronics.api.API;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -38,41 +42,71 @@ public class Addshippingaddress extends AppCompatActivity {
         btnSaveAddressAddShip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = edtNameAddShip.getText().toString();
+                String address = edtAddressAddShip.getText().toString();
+                String phonenumber = edtPhoneNumber.getText().toString();
+                String city = edtCityAddShip.getText().toString();
+                String district = edtDistrictAddShip.getText().toString();
+                String ward = edtWardAddShip.getText().toString();
+
                 if (edtNameAddShip.getText().toString().isEmpty()){
                     Toast.makeText(Addshippingaddress.this, "Bạn chưa nhâp họ và tên", Toast.LENGTH_SHORT).show();
                 }
-                if (edtAddressAddShip.getText().toString().isEmpty()){
+                 if (edtAddressAddShip.getText().toString().isEmpty()){
                     Toast.makeText(Addshippingaddress.this, "Bạn chưa nhập địa chỉ", Toast.LENGTH_SHORT).show();
                 }
-                if (edtPhoneNumber.getText().toString().isEmpty()){
+                 if (edtPhoneNumber.getText().toString().isEmpty()){
                     Toast.makeText(Addshippingaddress.this, "Bạn chưa nhâp số điện thoại", Toast.LENGTH_SHORT).show();
                 }
-                if (edtCityAddShip.getText().toString().isEmpty()){
+                 if (edtCityAddShip.getText().toString().isEmpty()){
                     Toast.makeText(Addshippingaddress.this, "Bạn chưa nhâp thành phố", Toast.LENGTH_SHORT).show();
                 }
-                if (edtDistrictAddShip.getText().toString().isEmpty()){
+                 if (edtDistrictAddShip.getText().toString().isEmpty()){
                     Toast.makeText(Addshippingaddress.this, "Bạn chưa nhâp quận/huyện", Toast.LENGTH_SHORT).show();
                 }
-                if (edtWardAddShip.getText().toString().isEmpty()){
+                 if (edtWardAddShip.getText().toString().isEmpty()){
                     Toast.makeText(Addshippingaddress.this, "Bạn chưa nhâp phường/xã", Toast.LENGTH_SHORT).show();
                 }
-//                new Thread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        handlerAddress.post(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                progressDialog = new ProgressDialog(Addshippingaddress.this);
-//                                progressDialog.setMessage("Loanding");
-//                                progressDialog.setCancelable(false);
-//                                progressDialog.show();
-//                            }
-//                        });
-//                        Retrofit retrofit = new Retrofit.Builder()
-//                                .baseUrl()
-//                                .addCallAdapterFactory(GsonConverterFactory.create());
-//                    }
-//                });
+
+                if((name.isEmpty() && address.isEmpty() && phonenumber.isEmpty() && city.isEmpty() && district.isEmpty() && ward.isEmpty()) ){
+                    Toast.makeText(Addshippingaddress.this, "hehe", Toast.LENGTH_SHORT).show();
+                }
+                else  {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            handlerAddress.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressDialog = new ProgressDialog(Addshippingaddress.this);
+                                    progressDialog.setMessage("Loanding");
+                                    progressDialog.setCancelable(false);
+                                    progressDialog.show();
+                                }
+                            });
+                            Retrofit retrofit = new Retrofit.Builder()
+                                    .baseUrl("https://khoihoang0511.000webhostapp.com/")
+                                    .addConverterFactory(GsonConverterFactory.create())
+                                    .build();
+                            API api = retrofit.create(API.class);
+                            Call<String> callDress = api.addDress(name,phonenumber,ward,district,city,address);
+                            callDress.enqueue(new Callback<String>() {
+                                @Override
+                                public void onResponse(Call<String> call, Response<String> response) {
+                                    if (response.isSuccessful() && response.body() != null){
+                                        progressDialog.dismiss();
+                                        Toast.makeText(Addshippingaddress.this, "Thêm giỏ hàng thành công", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(Call<String> call, Throwable t) {
+                                    Toast.makeText(Addshippingaddress.this, "Kết nối không ổn định", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }).start();
+                }
             }
         });
     }
