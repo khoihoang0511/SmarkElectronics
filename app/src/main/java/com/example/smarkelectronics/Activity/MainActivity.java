3df91296433a26a27c9e1e758ff8eca54133227d
ngocphat922004 <130296645+ngocphat922004@users.noreply.github.com>
2023-11-11 23:50:37 +0700

    yeuthich

--- app/src/main/java/com/example/smarkelectronics/Activity/MainActivity.java
+++ app/src/main/java/com/example/smarkelectronics/Activity/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.smarkelectronics.Activity;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.smarkelectronics.Fragment.HomeFragment;
 import com.example.smarkelectronics.R;
